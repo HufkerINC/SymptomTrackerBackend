77de34dfb0089f2e24e5bd2e866bdd8557a26609
Kira Nim <kiras.nim@gmail.com>
2021-05-05 13:15:11 +0200

    initial commit

---
+++ src/main/java/dk/symptomtracker/symptomtracker_backend/SymptomtrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package dk.symptomtracker.symptomtracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymptomtrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymptomtrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/symptomtracker/symptomtracker_backend/SymptomtrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.symptomtracker.symptomtracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymptomtrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
